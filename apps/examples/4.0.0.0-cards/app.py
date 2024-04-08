57cb9ab993acaca39681141ef313f5258574de4a
Piotr Pasza Storozenko <piotr@appsilon.com>
2024-04-08 18:05:44 +0200

    wip: slides adjusted

--- apps/examples/4.0.0.0-cards/app.py
+++ apps/examples/4.0.0.0-cards/app.py
@@ -1,11 +1,10 @@
 from shiny import Inputs, Outputs, Session, App, reactive, render, req, ui
-import shiny.experimental as x
 
 app_ui = ui.page_fluid(
     ui.column(
         3,
-        x.ui.card(
-            x.ui.card_header("Slider card"),
+        ui.card(
+            ui.card_header("Slider card"),
             ui.input_slider("n", "N", 0, 100, 20),
             ui.output_text_verbatim("txt"),
             fill=False,
